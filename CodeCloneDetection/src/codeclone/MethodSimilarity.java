b511de51996bb9795a88c0a93a1704de0a8a2845
liuqingli <liuqing@vt.edu>
2016-03-26 16:00:16 -0400

    Add test files and fix simTokenNum bug

--- CodeCloneDetection/src/codeclone/MethodSimilarity.java
+++ CodeCloneDetection/src/codeclone/MethodSimilarity.java
@@ -8,7 +8,8 @@ public class MethodSimilarity {
 	public double simMethodPara, simMethodType;
 	public double simTokenType, simTokenKeyword, simTokenOtherStr;
 	public double simTokenMarker, simTokenOperator, simTokenOtherChar;
-	public double w1 = 0.1, w2 = 0.1, w3 = 0.15, w4 = 0.15, w5 = 0.3, w6 = 0.05, w7 = 0.1, w8 = 0.05;
+	public double simTokenNum;
+	public double w1 = 0.1, w2 = 0.1, w3 = 0.1, w4 = 0.15, w5 = 0.3, w6 = 0.05, w7 = 0.1, w8 = 0.05, w9 = 0.05;
 	public double methodSimilarity;
 	
 	//public double tokenThreshold = 0.7;
@@ -110,6 +111,11 @@ public class MethodSimilarity {
 		tokenList2 = mVector2.methodTokenList.getListByType("OtherChar");
 		simTokenOtherChar = tokenListSim(tokenList1, tokenList2);
 		
+		// calculate token_Num's similarity 
+		tokenList1 = mVector1.methodTokenList.getListByType("Num");
+		tokenList2 = mVector2.methodTokenList.getListByType("Num");
+		simTokenNum = tokenListSim(tokenList1, tokenList2);
+		
 		/*
 		System.out.printf("{%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f}", simMethodPara, simMethodType,
 				simTokenType, simTokenKeyword, simTokenOtherStr, simTokenMarker, simTokenOperator, 
@@ -119,13 +125,7 @@ public class MethodSimilarity {
 		// calculate the similarity between two methods
 		return simMethodPara * w1 + simMethodType * w2 + simTokenType * w3 +
 				simTokenKeyword * w4 + simTokenOtherStr * w5 + simTokenMarker * w6 +
-				simTokenOperator * w7 + simTokenOtherChar * w8;
-		/*
-		System.out.printf("%15s%15s%15.3f\n", 
-				mList.getMethodVector(index1).methodName, 
-				mList.getMethodVector(index2).methodName, 
-				methodSimilarity);
-				*/
+				simTokenOperator * w7 + simTokenOtherChar * w8 + simTokenNum * w9;
 	}
 	
 	// code clone detector for a single java file
@@ -135,8 +135,8 @@ public class MethodSimilarity {
 				methodSimilarity = methodVectorSim(mList.getMethodVector(index1), mList.getMethodVector(index2));
 				// output clone group
 				if(methodSimilarity >= detectThreshold
-						&& mList.getMethodVector(index1).endLineNumber - mList.getMethodVector(index1).startLineNumber > 8
-						&& mList.getMethodVector(index2).endLineNumber - mList.getMethodVector(index2).startLineNumber > 8) {
+						&& mList.getMethodVector(index1).endLineNumber - mList.getMethodVector(index1).startLineNumber > 7
+						&& mList.getMethodVector(index2).endLineNumber - mList.getMethodVector(index2).startLineNumber > 7) {
 					//System.out.println("1");
 					System.out.println("Clone Group " + countID + " --> " +
 							"Similarity :" + String.format("%.4f", methodSimilarity));
@@ -163,8 +163,8 @@ public class MethodSimilarity {
 				methodSimilarity = methodVectorSim(mList1.getMethodVector(index1), mList2.getMethodVector(index2));
 				// output clone group
 				if(methodSimilarity >= detectThreshold
-						&& mList1.getMethodVector(index1).endLineNumber - mList1.getMethodVector(index1).startLineNumber > 8
-						&& mList2.getMethodVector(index2).endLineNumber - mList2.getMethodVector(index2).startLineNumber > 8) {
+						&& mList1.getMethodVector(index1).endLineNumber - mList1.getMethodVector(index1).startLineNumber > 7
+						&& mList2.getMethodVector(index2).endLineNumber - mList2.getMethodVector(index2).startLineNumber > 7) {
 					System.out.println("Clone Group " + countID + " --> " +
 							"Similarity :" + String.format("%.4f", methodSimilarity));
 					System.out.printf("%15s%15d%15d\n", 
